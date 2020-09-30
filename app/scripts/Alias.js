029c9526f8f76ebf3a71b5c113d8e859f2441924
SynGameDev <syndicategamedev@gmail.com>
2020-09-30 02:43:00 +0000

    Alias

---
+++ app/scripts/Alias.js
@@ -0,0 +1,7 @@
+let Application = PIXI.Application;
+let Loader = PIXI.loader;
+let Resources = PIXI.loader.resources
+let Sprite = PIXI.loader.resources;
+let Text = PIXI.Text;
+let TextStyle = PIXI.TextStyle;
+let Graphics = PIXI.Graphics;
